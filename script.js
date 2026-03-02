2ac844b37d3e2d0583bda9c873c0334b811ab7b4
khawajaacode <babarnaseer716@gmail.com>
2026-03-02 23:23:04 +0500

    feat: implement dark and light theme toggle

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Student Dashboard Loaded");
+
+const toggleBtn = document.getElementById("themeToggle");
+const body = document.body;
+
+toggleBtn.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+
+    if (body.classList.contains("dark-mode")) {
+        toggleBtn.textContent = "☀️";
+    } else {
+        toggleBtn.textContent = "🌙";
+    }
+});
